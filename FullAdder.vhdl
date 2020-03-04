d52fa58a5dc9cb01487afafa6c2182c225f17b35
PauMarquez99 <61257575+PauMarquez99@users.noreply.github.com>
2020-03-04 15:37:16 -0600

    Update FullAdder.vhdl

--- FullAdder.vhdl
+++ FullAdder.vhdl
@@ -1,10 +1,10 @@
 entity FullAdder is
-port(X, Y, Cin: in bit;
-    Cout, Suma: out bit);
+port(X, Y, Cin: in bit; --señales de entrada
+    Cout, Suma: out bit); --señales de salida
 end FullAdder;
 
-architecture Logic of FullAdder is
+architecture Logic of FullAdder is --comportamiento del FullAdder
 begin
-  Suma <= X xor Y xor Cin after 10 ns;
-  Cout <= (X and Y) or (X and Cin) or (Y and Cin) after 10 ns;
+  Suma <= X xor Y xor Cin after 10 ns; --ecuación de la suma
+  Cout <= (X and Y) or (X and Cin) or (Y and Cin) after 10 ns; --ecuación del carry out
 end Logic;
