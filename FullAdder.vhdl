5f80327472889ce51cf64465bd91e491d53c59dc
PauMarquez99 <61257575+PauMarquez99@users.noreply.github.com>
2020-03-04 19:19:49 -0600

    Update FullAdder.vhdl

--- FullAdder.vhdl
+++ FullAdder.vhdl
@@ -9,4 +9,10 @@ architecture archi of Full_Add is
     port(A, B, C, D: in bit;
       S1,S2: in bit;
       z: out bit);
-end component;
+  end component;
+signal cinnot : bit;
+begin
+  mux1: mux_4_1 port map(ci, cinnot, cinnot, ci, i0, i1, s);
+  mux2: mux_4_1 port map('0', ci, ci, '1', i0, i1, co);
+  cinnot <= NOT ci;
+end archi;
