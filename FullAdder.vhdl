a35e3e00984e3becd53113449469cf7637affba3
PauMarquez99 <61257575+PauMarquez99@users.noreply.github.com>
2020-03-04 19:06:58 -0600

    Update FullAdder.vhdl

--- FullAdder.vhdl
+++ FullAdder.vhdl
@@ -1,3 +1,12 @@
 entity Full_Add is
-
+  port (i0, i1 : in bit; ci : in bit;
+    s : out bit;
+    co : out bit);
 end Full_Add;
+
+architecture archi of Full_Add is
+  component mux_4_1
+    port(A, B, C, D: in bit;
+      S1,S2: in bit;
+      z: out bit);
+end component;
