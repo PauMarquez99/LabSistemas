6a22f6af151f58eca4e076cee390293af66988f7
PauMarquez99 <61257575+PauMarquez99@users.noreply.github.com>
2020-06-08 19:35:45 -0500

    Update bdc_seven_tb.vhdl

--- bdc_seven_tb.vhdl
+++ bdc_seven_tb.vhdl
@@ -3,16 +3,20 @@ end bdc_seven_tb;
 
 architecture behavioral of bdc_seven_tb is
   component bdc_seven is
-    port(bdc: in bit_vector(3 downto 0); seven: out bit_vector(7 downto 1));
+    port(EN, DEC: in bit; DECOUT: outbit;
+    bdc: in bit_vector(3 downto 0); seven: out bit_vector(7 downto 1));
   end component;
+  signal EN, DEC, DECOUT: bit;
   signal bdc: bit_vector(3 downto 0);
   signal seven: bit_vector(7 downto 1);
 
   begin
-  uut: bdc_seven port map(bdc, seven);
+  uut: bdc_seven port map(EN, DEC, DECOUT, bdc, seven);
 
   stim_proc: process
   begin
+    en <= '1';
+    DEC <= '1';
     bdc <= "0000";
     wait for 100 ns;
     bdc <= "0001";
