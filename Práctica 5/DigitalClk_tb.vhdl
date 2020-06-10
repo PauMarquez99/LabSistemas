2b0d1592acf19b46edb863bcdc5a94be06157079
PauMarquez99 <61257575+PauMarquez99@users.noreply.github.com>
2020-06-10 10:22:59 -0500

    Create DigitalClk_tb.vhdl

---
+++ Práctica 5/DigitalClk_tb.vhdl
@@ -0,0 +1,30 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity DigitalClk_tb is
+end DigitalClk_tb;
+
+architecture behavioural of DigitalClk_tb is
+  component DigitalClk
+    port(clk, setM, setH: in bit; segM: out unsigned(5 downto 0);
+    segH: out unsigned(3 downto 0); PM: out bit);
+  end component;
+
+  signal clk, setH, setM, PM: bit;
+  signal segM: unsigned (5 downto 0);
+  signal segH: unsigned (3 downto 0);
+
+  begin
+    uut1: DigitalClk port map(clk, setM, setH, segM, segH, PM);
+
+  process
+  begin
+    clk <= '1';
+    wait for 1 ns;
+    clk <= '0';
+    wait for 1 ns;
+  end process;
+
+  ´process(clk)
+  begin
