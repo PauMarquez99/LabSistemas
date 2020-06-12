9a026ffa3dfb83843ecd42701b8da0d070aa31da
PauMarquez99 <61257575+PauMarquez99@users.noreply.github.com>
2020-06-12 12:57:23 -0500

    Update ShiftRegister74.vhdl

--- BitRegister8/ShiftRegister74.vhdl
+++ BitRegister8/ShiftRegister74.vhdl
@@ -14,4 +14,4 @@ architecture behavioural of ShiftRegister74 is
   begin
     LogicA <= (SR and not(S1) and S0) or (A and S1 and S0) or (SQB and not(S0) and S1) or (S0 and not(S1) and SQA);
     LogicB <= (SQA and not (S1) and S0) or (S0 and S1 and B) or (not (S0) and S1 and SQC) or (not (S0) and not (S1) and SQB);
-     
+    LogicC <= (SQB and )
