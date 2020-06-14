af3a3e93196623e71c027639507e4b95fc718877
PauMarquez99 <61257575+PauMarquez99@users.noreply.github.com>
2020-06-13 21:09:42 -0500

    Update ShiftRegister74.vhdl

--- BitRegister8/ShiftRegister74.vhdl
+++ BitRegister8/ShiftRegister74.vhdl
@@ -1,17 +1,29 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
 
-entity ShiftRegister74 is
-  port(A, B, C, D, S1, S0, SR, SL, Clk, Clr: in std_logic; QA, QB, QC, QD: out bit);
-end entity;
-
-architecture behavioural of ShiftRegister74 is
-
-  signal LogicA, LogicB, LogicC, LogicD: std_logic;
-  signal SQA, SQB, SQC, SQD: std_logic;
-
+entity ls194 is
+  port(clk,mr_n,s0,s1,dsr,dsl_ser: in std_logic;
+  p : in std_logic_vector(3 downto 0);
+  q : out std_logic_vector(3 downto 0)
+  );
+end ls194;
+architecture behav of ls194 is
+  signal temp: std_logic_vector(3 downto 0);
+  signal ctrl: std_logic_vector (1 downto 0);
   begin
-    LogicA <= (SR and not(S1) and S0) or (A and S1 and S0) or (SQB and not(S0) and S1) or (S0 and not(S1) and SQA);
-    LogicB <= (SQA and not (S1) and S0) or (S0 and S1 and B) or (not (S0) and S1 and SQC) or (not (S0) and not (S1) and SQB);
-    LogicC <= (SQB and )
+    ctrl <= s0 & s1;
+    shift_proc: process(clk, mr_n )
+    begin
+      if (mr_n = '0') then
+        temp <= (others => '0');
+      elsif (rising_edge(clk)) then
+      case ctrl is
+        when "11" => temp <= p;
+        when "10" => temp <= dsr & temp(3 downto 1);
+        when "01"=> temp <= temp(2 downto 0) & dsl;
+        when others => temp <= temp;
+      end case;
+      end if;
+    end process;
+    q <= temp;
+end behav;
