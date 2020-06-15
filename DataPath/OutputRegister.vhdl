0a03262c3adfeec3f72fa4ab5727ab33da54cf33
PauMarquez99 <61257575+PauMarquez99@users.noreply.github.com>
2020-06-14 22:27:00 -0500

    Create OutputRegister.vhdl

---
+++ DataPath/OutputRegister.vhdl
@@ -0,0 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity OutputRegister is
+  port(O_enb: in std_logic;
+    input: in std_logic_vector(3 downto 0);
+    Data_O: out std_logic_vector(3 downto 0)
+  );
+end entity;
+
+architecture log of OutputRegister is
+
+begin
+  
+
+end log;
