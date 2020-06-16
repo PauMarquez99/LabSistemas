2671c4ee7450ba90f284b92a76b3d20554e44f00
PauMarquez99 <61257575+PauMarquez99@users.noreply.github.com>
2020-06-16 00:41:45 -0500

    Keyboard Error Updates

--- Keyboard/keyboard2_tb.vhdl
+++ Keyboard/keyboard2_tb.vhdl
@@ -7,12 +7,12 @@ library std;
 use std.textio.all;
 
 
-entity keyboard_tb is
-    constant period : time := 40 ns ; -- Señal de reloj de 25MHz
+entity keyboard2_tb is
+    constant period     : time := 40 ns ; -- Señal de reloj de 25MHz
     constant bit_period : time := 60 us ; -- Keyboard clock ~ 16.7 Khz max
 end entity;
 
-architecture arch of keyboard_tb is
+architecture arch of keyboard2_tb is
     component keyboard2 is
         port(
             kbd_clk, kbd_data, clk  : in std_logic;
@@ -28,32 +28,40 @@ architecture arch of keyboard_tb is
     signal kbd_clk    : std_logic := '1';
     signal kbd_data   : std_logic := 'H';
     signal enable     : std_logic := '0';
-    signal scan_ready : std_logic;
+    signal scan_ready : std_logic := '0';
+    signal parity_bit : std_logic := '0';
     signal scan_code  : std_logic_vector(7 downto 0);
 
     type codes_types is array (natural range <>) of std_logic_vector (7 downto 0);
-    constant codes : codes_types := (x"15", x"1D");
+    type input_type is record
+      codes   : std_logic_vector(7 downto 0);
+      parity  : std_logic;
+    end record input_type;
+    type out_code is array (natural range <>) of input_type;
+    constant codes_constant : out_code := ((x"15", '0'), (x"1D", '0'));
 
     begin
 
-    UUT : keyboard port map (kbd_clk, kbd_data, clk, reset, enable, scan_code, scan_ready );
+    UUT : keyboard2 port map (kbd_clk, kbd_data, clk, reset, enable, scan_code, scan_ready, parity_bit);
 
     -- Señal de reloj del sistema
     clk <= not clk after (period / 2);
     reset <= '1', '0' after period;
 
     process
-        procedure send_code( sc : std_logic_vector(7 downto 0) ) is
+        procedure send_code( sc : std_logic_vector(7 downto 0); parity_know : std_logic ) is
         begin
             kbd_clk <= 'H';
             kbd_data <= 'H';
 
             wait for (bit_period/2);
-            kbd_data <= '0'; -- Start bit
+            kbd_data <= '0';
+             -- Start bit
             wait for (bit_period/2);
             kbd_clk <= '0';
             wait for (bit_period/2);
             kbd_clk <= '1';
+
             for i in 0 to 7 loop
                 kbd_data <= sc(i);
                 wait for (bit_period/2);
@@ -61,12 +69,14 @@ architecture arch of keyboard_tb is
                 wait for (bit_period/2);
                 kbd_clk <= '1';
             end loop;
+
             -- bit de paridad
             kbd_data <= '0';
             wait for (bit_period/2);
             kbd_clk <= '0';
             wait for (bit_period/2);
             kbd_clk <= '1';
+
             -- stop bit
             kbd_data <= '1';
             wait for (bit_period/2);
@@ -79,8 +89,8 @@ architecture arch of keyboard_tb is
 
        begin
         wait for bit_period;
-        for i in codes'range loop
-            send_code(codes(i));
+        for i in codes_constant'range loop
+            send_code(codes_constant(i).codes, codes_constant(i).parity);
         end loop;
     end process;
 
@@ -91,6 +101,9 @@ architecture arch of keyboard_tb is
         wait for 300* period;
         write (l, string'("Scan code : "));
         write (l, scan_code);
+        if parity_bit = '1' then
+          write (l, string'("Parity Bit : "));
+        end if;
         writeline(output, l);
         wait for bit_period;
     end process;
